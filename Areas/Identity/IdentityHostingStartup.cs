5cef1f4bec54cb7d3419af9a660860d8829e04eb
liam-is-working <vulam270403@gmail.com>
2021-06-27 12:00:27 +0700

    scaffold identity

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+using System;
+using Childcare.Areas.Identity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Childcare.Areas.Identity.IdentityHostingStartup))]
+namespace Childcare.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ChildCareContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ChildCareContextConnection")));
+
+                services.AddDefaultIdentity<ChildCareUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ChildCareContext>();
+            });
+        }
+    }
+}
